b4d2196f21d0d3da46269a0eb8be92e502fb11b0
Fatma <shauqifatma@gmail.com>
2021-12-13 19:17:19 +0100

    we add a list

---
+++ index.js
@@ -0,0 +1,25 @@
+var arry = [
+  {
+    fristName: "fatma",
+    lastName: "shauqi",
+  },
+  {
+    fristName: "oday",
+    lastName: "ajam",
+  },
+  {
+    fristName: "greg",
+    lastName: "thomas",
+  },
+];
+console.log(arry);
+
+for (var i = 0; i < arry.length; i++) {
+  var names = arry[i];
+  console.log(names);
+}
+
+/*function getList(arry){
+    return "<ul>" + ''+ "<li>"+fristName+ "</li><li>"+ lastName+ "<li><</ul>"
+
+}*/
