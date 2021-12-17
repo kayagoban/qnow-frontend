a6dacd0a2047b5dc24ee19231b5c84bf73c7df84
Fatma <shauqifatma@gmail.com>
2021-12-17 19:06:01 +0100

    change function to load body page

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ var arry = [
   },
 ];
 console.log(arry);
+window.addEventListener("load", function () {
+  getList(arry);
+});
 
 function getList(arry) {
   var info = "<ul>";
   for (var i = 0; i < arry.length; i++) {
     var person = arry[i];
-    var personName = fristName;
-    var personLastName = lastName;
+    var personName = person.fristName;
+    var personLastName = person.lastName;
     var personHtml =
       "<li>" + personName + "</li>" + "<li>" + personLastName + "</li>";
     info = info + personHtml;
